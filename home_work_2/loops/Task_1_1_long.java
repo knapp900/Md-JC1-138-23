2855e61c9df862b1ccc6a1edfde190e07cb528cc
Arthur <40965835+knapp900@users.noreply.github.com>
2023-10-02 01:21:45 +0300

    Add files via upload

---
+++ home_work_2/loops/Task_1_1_long.java
@@ -0,0 +1,158 @@
+package home_work_2.loops;
+
+/*
+1.1. Перемножить числа от 1 до числа (включительно) введенного через аргумент к исполняемой программе.
+ Есть нюанс с переполнением, можно добавить проверки и сообщения пользователю.
+ Пример: Ввели 5, должно получиться в консоли: 1 * 2 * 3 * 4 * 5 = ответ
+ */
+
+public class Task_1_1_long {
+    private String request;
+    private int iterator;
+    private long result;
+
+    public static void main(String[] args) {
+
+        Task_1_1_long app = new Task_1_1_long();
+
+        app.request = app.getFirstArgument(args); // Читает аргументы
+
+        if (app.isNumber(app.request)) { // Проверка args что это число и оно не начинается с нуля
+
+            app.iterator = app.stringToInt(app.request); // Присваиваем args переменной long
+
+            app.result = app.factorial(app.iterator); // Присваиваем результаты вычисления переменной
+
+            if (app.result != 0) {
+
+                app.printMessage(app.buildAnswer(app.result, app.iterator)); // Выводим в консоль результат
+
+            } else {
+
+                app.printMessage("Число слишком длинное! long is overflow!");
+            }
+
+        } else {
+
+            app.printMessage("Введены неверные данные или аргументы пусты");
+        }
+
+    }
+
+    /**
+     * ATTENTION! может вернуть NULL!
+     * Метод возвращает первый аргумент командной строки
+     *
+     * @param args массив аргументов
+     * @return если !null возвращает первый аргумент если null то вернет null
+     */
+    private String getFirstArgument(String[] args) {
+
+        if (args != null) {
+
+            for (String arguments : args) {
+
+                return arguments;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Метод преобразует String to int
+     *
+     * @param stringOfNumber String содержащий число
+     * @return int результат преобразования строки
+     */
+    private int stringToInt(String stringOfNumber) {
+
+        return Integer.parseInt(stringOfNumber);
+    }
+
+    /**
+     * Метод высчитывает факториал переданного числа
+     *
+     * @param number = факториал какого числа нужно найти
+     * @return = возвращает 0 если long переполнен и result если вычисление прошло успешно
+     */
+    private long factorial(int number) {
+
+        long result = 1;
+
+        for (int i = 1; i <= number; i++) {
+
+            result *= i;
+
+            if (!isNotOverflow(result)) {
+                return 0;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Метод проверяет не переполнен ли long
+     *
+     * @param number = проверяемое число
+     * @return = true если long не переполнен и false если переполнен
+     */
+    private boolean isNotOverflow(long number) {
+
+        return number < Long.MAX_VALUE && number > 0;
+    }
+
+    /**
+     * Метод проверяет является ли содержимое строки числом и не равняется нулю
+     *
+     * @param args = строка для проверки на число
+     * @return true если строка содержит число и не начинается с нуля
+     */
+    private boolean isNumber(String args) {
+
+        if (args == null) {
+
+            return false;
+        }
+
+        return args.matches("^[0-9]+$") && args.charAt(0) != '0';
+    }
+
+    /**
+     * Метод выводит на печать любую строку
+     *
+     * @param message = строка которая должна быть напечатана в консоль
+     */
+    private void printMessage(String message) {
+
+        System.out.println(message);
+    }
+
+    /**
+     * Метод формирует ответ в заданном формате (1*2*3=6)
+     *
+     * @param result   принимает long результат который будет находится после знака равенства
+     * @param iterator принимает int количество символов для формирования строки до знака равенства
+     * @return возвращает String в заданном формате
+     */
+    private String buildAnswer(long result, int iterator) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 1; i <= iterator; i++) {
+
+            builder.append(i);
+            builder.append('*');
+
+        }
+
+        builder.deleteCharAt(builder.length() - 1);
+        builder.append('=');
+        builder.append(result);
+
+        return builder.toString();
+
+    }
+
+
+}
---
+++ home_work_2/loops/Task_1_2.java
@@ -0,0 +1,139 @@
+package home_work_2.loops;
+
+/*
+1.2. Все цифры из числа введенного через аргумент к исполняемой программе перемножить между собой и вывести
+ход вычислений в консоль. Пользователь обязан ввести целое число. Если ввели не целое то выдать сообщение о
+том что пользователь ввёл некорректные данные.
+    1.2.1 Пример: Ввели 181232375, должно получиться в консоли: 1 * 8 * 1 * 2 * 3 * 2 * 3 * 7 * 5 = 10 080
+    1.2.2 Пример: Ввели 99.2, должно получиться в консоли: Введено не целое число
+    1.2.3 Пример: Ввели Привет, должно получиться в консоли: Введено не число
+ */
+
+public class Task_1_2 {
+    private String request;
+    int[] arrayDigitsOfString;
+
+    public static void main(String[] args) {
+
+        Task_1_2 app = new Task_1_2();
+        // Читает аргументы
+        for (String arguments : args) app.request = arguments; // Проверка args что это число и оно не начинается с нуля
+
+        if (app.isNumber(app.request)) {
+            if (!app.isFlout(app.request)) {
+
+                app.arrayDigitsOfString = app.stringToIntArray(app.request);
+                app.printMessage(
+                        app.buildAnswer(
+                                app.arrayDigitsOfString,app.multiplyingDigitsOfNumber(app.arrayDigitsOfString)));
+
+            } else {
+
+                app.printMessage("Введено не целое число");
+            }
+        } else {
+
+            app.printMessage("Введено не число");
+        }
+
+    }
+
+    /**
+     * Метод переводит цифры из строки в массив int
+     *
+     * @param number строка которую нужно перевести в массив
+     * @return массив с результатом
+     */
+    private int[] stringToIntArray(String number) {
+
+        int[] arrayOfDigitv = new int[number.length()];
+
+        for (int i = 0; i < number.length(); i++) {
+            // charAt ASCII вычитая 48 из результата получаем десятичное представление числа
+            arrayOfDigitv[i] = (number.charAt(i) - 48);
+        }
+        return arrayOfDigitv;
+    }
+
+    /**
+     * Метод перемножает цифры переданного числа*
+     *
+     * @param number массив чисел
+     * @return long с результатом
+     */
+    private long multiplyingDigitsOfNumber(int[] number) {
+
+        long result = 1;
+
+        for (int i = 1; i <= number.length-1; i++) {
+
+            result *= number[i];
+
+        }
+
+        return result;
+    }
+
+    /**
+     * Метод проверяет является ли содержимое строки числом и не равняется нулю
+     *
+     * @param args = строка для проверки на число
+     * @return true если строка содержит число и не начинается с нуля
+     */
+    private boolean isNumber(String args) {
+
+        return args.matches
+                ("^[0-9]+$") // Проверяет что в строке только число
+                || args.matches("^([0-9]+\\.[0-9]+)$") // Проверяет что в строке только число с точкой
+                && args.charAt(0) != '0'; // Проверяет что в строка не начинается с нуля
+
+    }
+
+    /**
+     * Метод проверяет является ли число вещественным
+     *
+     * @param args строка содержащая число для проверки
+     * @return true если число с плавающей точкой и false если без
+     */
+    private boolean isFlout(String args) {
+
+        return  args.matches("^([0-9]+\\.[0-9]+)$");
+    }
+
+
+    /**
+     * Метод выводит на печать любую строку
+     *
+     * @param message = строка которая должна быть напечатана в консоль
+     */
+    private void printMessage(String message) {
+
+        System.out.println(message);
+    }
+
+    /**
+     * Метод выводит результат в консоль в заданном формате (1*2*3=6)
+     *
+     * @param arrayOfDigit   результат вычисления факториала
+     * @param result число из которого вычисляли факториал
+     */
+    private String buildAnswer(int[] arrayOfDigit, long result) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i <= arrayOfDigit.length-1; i++) {
+
+            builder.append(arrayOfDigit[i]);
+            builder.append('*');
+
+        }
+
+        builder.deleteCharAt(builder.length() - 1);
+        builder.append('=');
+        builder.append(result);
+
+        return builder.toString();
+
+    }
+
+}
