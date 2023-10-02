4e7291de3309dfe60057c22f2a234bf8d7df2c47
Arthur Knapp <knapp900@gmai.com>
2023-10-02 15:36:10 +0300

    fix task2_3_maiin

--- src/home_work_2/arrays/Task_2_2.java
+++ src/home_work_2/arrays/Task_2_2.java
@@ -9,36 +9,7 @@ import home_work_2.utils.ArraysUtils;
 
 public class Task_2_2 {
     public static void main(String[] args) {
-        IArraysOperation doWhileOps = new DoWhileOperation();
-        IArraysOperation whileOps = new WhileOperation();
-        IArraysOperation forEachOps = new ForEachOperation();
-        IArraysOperation forOps = new ForOperation();
 
-
-        doWhileOps.printAllElements(ArraysUtils.arrayRandom(10,100));
-        System.out.println();
-        doWhileOps.printEverySecondElement(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
-        doWhileOps.printElementsInReverseOrder(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
-        whileOps.printAllElements(ArraysUtils.arrayRandom(10,10));
-        System.out.println();
-        whileOps.printEverySecondElement(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
-        whileOps.printElementsInReverseOrder(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
-        forEachOps.printAllElements(ArraysUtils.arrayRandom(10,100));
-        System.out.println();
-        forEachOps.printEverySecondElement(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
-        forEachOps.printElementsInReverseOrder(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
-        forOps.printAllElements(ArraysUtils.arrayRandom(10,100));
-        System.out.println();
-        forOps.printEverySecondElement(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
-        forOps.printElementsInReverseOrder(new int[]{1,2,3,4,5,6,7,8,9,10});
-        System.out.println();
         ArrayIteration.printAllElements(ArraysUtils.arrayRandom(10,100));
         System.out.println();
         ArrayIteration.printEverySecondElement(new int[]{1,2,3,4,5,6,7,8,9,10});
---
+++ src/home_work_2/arrays/task_2_3/Task_2_3.java
@@ -0,0 +1,45 @@
+package home_work_2.arrays.task_2_3;
+
+import home_work_2.arrays.task_2_3.IArraysOperation;
+import home_work_2.arrays.task_2_3.impl.DoWhileOperation;
+import home_work_2.arrays.task_2_3.impl.ForEachOperation;
+import home_work_2.arrays.task_2_3.impl.ForOperation;
+import home_work_2.arrays.task_2_3.impl.WhileOperation;
+import home_work_2.utils.ArraysUtils;
+
+public class Task_2_3 {
+
+    public static void main(String[] args) {
+
+        IArraysOperation doWhileOps = new DoWhileOperation();
+        IArraysOperation whileOps = new WhileOperation();
+        IArraysOperation forEachOps = new ForEachOperation();
+        IArraysOperation forOps = new ForOperation();
+
+
+        doWhileOps.printAllElements(ArraysUtils.arrayRandom(10, 100));
+        System.out.println();
+        doWhileOps.printEverySecondElement(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+        doWhileOps.printElementsInReverseOrder(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+        whileOps.printAllElements(ArraysUtils.arrayRandom(10, 10));
+        System.out.println();
+        whileOps.printEverySecondElement(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+        whileOps.printElementsInReverseOrder(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+        forEachOps.printAllElements(ArraysUtils.arrayRandom(10, 100));
+        System.out.println();
+        forEachOps.printEverySecondElement(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+        forEachOps.printElementsInReverseOrder(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+        forOps.printAllElements(ArraysUtils.arrayRandom(10, 100));
+        System.out.println();
+        forOps.printEverySecondElement(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+        forOps.printElementsInReverseOrder(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        System.out.println();
+    }
+}
